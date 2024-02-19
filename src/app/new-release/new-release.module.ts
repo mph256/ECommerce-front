59b6771dc92fbdaba68e8ca64e7c66e7c87795c7
m <phamichael123@gmail.com>
2024-02-19 12:31:16 +0100

    feat: add new release routing

--- src/app/new-release/new-release.module.ts
+++ src/app/new-release/new-release.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { NewReleaseRoutingModule } from './new-release-routing.module';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    NewReleaseRoutingModule,
     SharedModule
   ]
 })
