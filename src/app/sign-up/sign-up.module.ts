c2d7065e36b1d382e152a5d1606ed01143f734f6
m <phamichael123@gmail.com>
2024-02-19 07:51:41 +0100

    feat: add sign-up routing

--- src/app/sign-up/sign-up.module.ts
+++ src/app/sign-up/sign-up.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { SignUpRoutingModule } from './sign-up-routing.module';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
@@ -9,6 +10,7 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    SignUpRoutingModule,
     SharedModule
   ]
 })
