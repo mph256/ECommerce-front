35e7fa8bad77e2abb50eed9661890d9d0d1c3921
m <phamichael123@gmail.com>
2024-02-19 09:20:21 +0100

    feat: add product module

---
+++ src/app/product/product.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    SharedModule
+  ],  
+  providers: []
+})
+export class ProductModule { }
