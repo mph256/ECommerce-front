a3e1af463acd3ba51ba1fabef1e8fcb2c142489e
m <phamichael123@gmail.com>
2024-02-19 07:33:46 +0100

    feat: add page not found component

--- src/app/page-not-found/page-not-found.module.ts
+++ src/app/page-not-found/page-not-found.module.ts
@@ -4,8 +4,12 @@ import { CommonModule } from '@angular/common';
 import { PageNotFoundRoutingModule } from './page-not-found-routing.module';
 import { SharedModule } from '../shared/shared.module';
 
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
 @NgModule({
-  declarations: [],
+  declarations: [
+    PageNotFoundComponent
+  ],
   imports: [
     CommonModule,
     PageNotFoundRoutingModule,
